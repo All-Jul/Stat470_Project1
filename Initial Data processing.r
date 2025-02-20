83d27d151d952ae331a5450189bc4c72f4f9dcbd
Allan Julian <aej5327@psu.edu>
2025-02-20 12:46:02 -0500

    Changed Capitalziation proble

--- Initial Data processing.r
+++ Initial Data processing.r
@@ -1,7 +1,9 @@
 library(readxl)
 library(dplyr)
+library(plyr)
 rm(list = ls())
 path_to_file <- "/Users/allan/Downloads/PenguinDataSP25.xlsx"
 PenguinData <- subset(read_excel(path_to_file, skip = 5) %>% filter(row_number() <= n()-4), select = -c(sampleID, scientific, health_metrics)) %>% filter(life_stage == 'adult' & body_mass != 0)
 PenguinData <- subset(PenguinData, select = -c(life_stage))
+PenguinData$species <- revalue(PenguinData$species, c('adelie'= 'Adelie'))
 PenguinData
