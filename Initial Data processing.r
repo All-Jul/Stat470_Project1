fb1bfefb65595b25fcb93165dc5d220749457cbe
Allan Julian <aej5327@psu.edu>
2025-02-20 12:39:49 -0500

    Droppped column

--- Initial Data processing.r
+++ Initial Data processing.r
@@ -1,6 +1,7 @@
 library(readxl)
 library(dplyr)
-rm(list=ls())
+rm(list = ls())
 path_to_file <- "/Users/allan/Downloads/PenguinDataSP25.xlsx"
 PenguinData <- subset(read_excel(path_to_file, skip = 5) %>% filter(row_number() <= n()-4), select = -c(sampleID, scientific, health_metrics)) %>% filter(life_stage == 'adult' & body_mass != 0)
+PenguinData <- subset(PenguinData, select = -c(life_stage))
 PenguinData
